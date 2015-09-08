b226fdf3a60fe482c1cfe919769bbee26b75994a
João Justo <jpjustonunes@hotmail.com>
2015-09-08 19:21:36 +0100

    Refactor code

--- index.js
+++ index.js
@@ -2,21 +2,38 @@
 
 var gm = require('gm');
 
-gm('./template3.jpg')
-  .fill('#414141')
-  .font('./Futura Bold.ttf')
-  .drawText(0, -50, 'PIOTR', 'Center')
-  .font('./futura.ttf')
-  .drawText(0, 50, 'SZOTKOWSKI', 'Center')
-  .font('./Adobe Garamond Bold.ttf')
-  .fill('#50ABF1')
-  .drawText(0, 180, '@reblz', 'Center')
-  .fontSize('70')
-  .write('./test.png', function(error) {
-    if (error) {
-      return console.log(error);
-    }
-    else {
-      return console.log('success');
-    }
-  });
+var fontSize = '70';
+var outputPath = './output/';
+var template = './images/template.jpg';
+var color = { blue: '#50ABF1', black: '#414141' };
+
+var futura = './fonts/futura.ttf';
+var futuraBold = './fonts/futura_bold.ttf';
+var garamondBold = './fonts/garamond_bold.ttf';
+
+function onEnd(error) {
+  if (error) {
+    console.log(error);
+  }
+  else {
+    console.log('Success');
+  }
+}
+
+function test() {
+  var fileName = 'test.jpg';
+
+  gm(template)
+    .fill(color.black)
+    .font(futuraBold)
+    .drawText(0, -50, 'PIOTR', 'Center')
+    .font(futura)
+    .drawText(0, 50, 'SZOTKOWSKI', 'Center')
+    .font(garamondBold)
+    .fill(color.blue)
+    .drawText(0, 180, '@reblz', 'Center')
+    .fontSize(fontSize)
+    .write(outputPath + fileName, onEnd);
+}
+
+test();
