cdd512aa57ddb235f6cffa1ba248956c9d22fdd5
João Justo <jpjustonunes@gmail.com>
2016-09-20 13:14:03 +0100

    Refactor to use some es6 goodness

--- index.js
+++ index.js
@@ -1,96 +1,99 @@
-var fs = require('fs');
-var gm = require('gm');
-var csv = require('csv-parser');
+const fs = require('fs');
+const gm = require('gm');
+const csv = require('csv-parser');
 
-var fontSize = 100;
-var fontSizeStaff = 120;
-var fontSizeSpeaker = 120;
-var outputPath = './output/';
+const outputPath = './output/';
 
-var attendePostion = {
-  firstName: { x: 100, y: 550 },
-  lastName:  { x: 100, y: 650 }
-}
-
-var speakerPostion = {
-  firstName: { x: 150, y: 610 },
-  lastName:  { x: 150, y: 760 }
-}
-
-var staffPostion = {
-  firstName: { x: 150, y: 610 },
-  lastName:  { x: 150, y: 760 }
-}
-
-var staffTemplate = './images/staff.jpg';
-var speakerTemplate = './images/speaker.jpg';
-var attendeTemplate = './images/attende.jpg';
-
-var color = { blue: '#50ABF1', black: '#414141', white: '#ffffff', pink: '#d42d4f' };
-
-var futura = './fonts/futura.ttf';
-var futuraBold = './fonts/futura_bold.ttf';
-var garamondBold = './fonts/garamond_bold.ttf';
-var montserratBold = './fonts/Montserrat-Bold.ttf';
-
-Function.prototype.curry = function() {
-    if (arguments.length < 1) {
-      return this; //nothing to curry with - return function
-    }
-    var __method = this;
-    var args = Array.prototype.slice.call(arguments);
-    return function() {
-      return __method.apply(this, args.concat(Array.prototype.slice.call(arguments)));
-    };
+const colors = {
+  pink: '#d4224f',
+  blue: '#50ABF1',
+  black: '#414141',
+  white: '#ffffff',
 };
 
-function onEnd(error) {
-  if (error) {
-    console.log(error);
-  }
+const staffTemplate = './images/staff.jpg';
+const speakerTemplate = './images/speaker.jpg';
+const attendeTemplate = './images/attende.jpg';
+
+const futura = './fonts/futura.ttf';
+const futuraBold = './fonts/futura_bold.ttf';
+const garamondBold = './fonts/garamond_bold.ttf';
+const montserratBold = './fonts/Montserrat-Bold.ttf';
+
+const attendeDefs = {
+  font: montserratBold,
+  fontSize: 100,
+  color: colors.white,
+  firstName: { x: 100, y: 550 },
+  lastName:  { x: 100, y: 650 },
+  textAlign: 'Left',
+  colorSpace: 'CMYK'
 }
 
-function draw(template, outputFolder, person) {
-  var fileName = person.firstName + '_' + person.id + '.jpg';
+const speakerDefs = {
+  font: montserratBold,
+  fontSize: 120,
+  color: colors.white,
+  firstName: { x: 150, y: 610 },
+  lastName:  { x: 150, y: 760 },
+  textAlign: 'Left',
+  colorSpace: 'CMYK'
+}
+
+const staffDefs = {
+  font: montserratBold,
+  fontSize: 120,
+  color: colors.pink,
+  firstName: { x: 150, y: 610 },
+  lastName:  { x: 150, y: 760 },
+  textAlign: 'Left',
+  colorSpace: 'CMYK'
+}
+
+const onEnd = error => {
+  if (error) console.log(error);
+}
+
+const draw = (template, outputFolder, defs) => person => {
+  const fileName = `${person.firstName}_${person.id}.jpg`;
 
   gm(template)
-    .fontSize(fontSizeStaff)
-    .fill(color.black)
-    .font(montserratBold)
-    .drawText(staffPostion.firstName.x, staffPostion.firstName.y, person.firstName.toUpperCase(), 'Left')
-    .drawText(staffPostion.lastName.x, staffPostion.lastName.y, person.lastName.toUpperCase(), 'Left')
-    .write(outputPath + outputFolder + '/' + fileName, onEnd);
+    .colorspace(defs.colorSpace)
+    .fontSize(defs.fontSize)
+    .fill(defs.color)
+    .font(defs.font)
+    .drawText(defs.firstName.x, defs.firstName.y, person.firstName.toUpperCase(), defs.textAlign)
+    .drawText(defs.lastName.x, defs.lastName.y, person.lastName.toUpperCase(), defs.textAlign)
+    .write(`${outputPath}${outputFolder}/${fileName}`, onEnd);
 }
 
-function cleanTwitter(person) {
+const cleanTwitter = person => {
   person.twitter = person.twitter.replace('@', '');
   person.twitter = person.twitter.replace('-', '');
 
-  if (person.twitter.length)
-    person.twitter = '@' + person.twitter;
+  if (person.twitter.length) person.twitter = '@' + person.twitter;
 
   return person;
 }
 
-function createCredential(template, outputFolder, credentials) {
+const createCredential = (template, outputFolder, credentials, defs) => () => {
   credentials = credentials.map(cleanTwitter);
-  credentials.forEach(draw.curry(template, outputFolder));
+  credentials.forEach(draw(template, outputFolder, defs));
 }
 
-function createCredentialsFromFile(template, file, outputFolder) {
-  var credentials = [];
+const createCredentialsFromFile = (template, file, outputFolder, defs) => {
+  const credentials = [];
+  const csvOptions = {
+    separator: ';',
+    headers: ['id', 'firstName', 'lastName', 'twitter']
+  };
 
   fs.createReadStream(file)
-    .pipe(csv({
-      separator: ';',
-      headers: ['id', 'firstName', 'lastName', 'twitter']
-    }))
-    .on('data', function(data) { //don't know why I cannot simply call like this: .on('data' credentials.push)
-      credentials.push(data);
-    })
-    .on('end', createCredential.curry(template, outputFolder, credentials));
+    .pipe(csv(csvOptions))
+    .on('data', data => credentials.push(data))
+    .on('end', createCredential(template, outputFolder, credentials, defs));
 }
 
-createCredentialsFromFile(staffTemplate, './staff.csv', 'staff');
-// createCredentialsFromFile(speakerTemplate, './speakers.csv', 'speakers');
-// createCredentialsFromFile(attendeTemplate, './attendes.csv', 'attendes');
+// createCredentialsFromFile(staffTemplate, './staff.csv', 'staff', staffDefs);
+// createCredentialsFromFile(speakerTemplate, './speakers.csv', 'speakers', speakerDefs);
+createCredentialsFromFile(attendeTemplate, './attendes.csv', 'attendes', attendeDefs);
